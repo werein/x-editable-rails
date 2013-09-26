23588e7c874435f7685e989010200e246f96c224
JiriKolarik <jiri.kolarik@imin.cz>
2013-09-26 22:16:32 +0200

    Accept title and type as option

--- lib/x-editable-rails/version.rb
+++ lib/x-editable-rails/version.rb
@@ -1,7 +1,7 @@
 module X
   module Editable
     module Rails
-      VERSION = "1.2.2"
+      VERSION = "1.3.0"
     end
   end
 end
--- lib/x-editable-rails/view_helpers.rb
+++ lib/x-editable-rails/view_helpers.rb
@@ -8,17 +8,15 @@ module X
           if can? :edit, object and xeditable?
             model = object.class.to_s.downcase
             model_param = model.gsub('::', '_')
-            model_name = model.gsub('::', '/')
             klass = options[:nested] ? object.class.const_get(options[:nested].to_s.singularize.capitalize) : object.class
-            content_tag :a, href: "#", class: "editable",
+            content_tag :a, href: '#', class: 'editable', title: options.fetch(:title, klass.human_attribute_name(method)),
             data: { 
-              type: 'text', 
+              type: options.fetch(:type, 'text'), 
               model: model_param, 
               name: method, 
               url: data_url, 
-              nested: (options[:nested] if options[:nested]), 
-              nid: (options[:nid] if options[:nid]), 
-              :'original-title' => klass.human_attribute_name(method)
+              nested: options[:nested], 
+              nid: options[:nid]
             } do
                 object.send(method) 
             end
