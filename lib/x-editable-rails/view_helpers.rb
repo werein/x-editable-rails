6fe734d5f3c507eeeca82666adad3c649a403bbc
Joel Van Horn <joel@joelvanhorn.com>
2013-10-26 17:38:50 -0400

    Renamed variables

--- lib/x-editable-rails/view_helpers.rb
+++ lib/x-editable-rails/view_helpers.rb
@@ -3,24 +3,24 @@ module X
     module Rails
       module ViewHelpers  
         def editable(object, method, options = {})
-          data_url = polymorphic_path(object)
           object   = object.last if object.kind_of?(Array)
           
           safe_value = value = object.send(method)
           safe_value = value.html_safe if value.respond_to? :html_safe
+          url     = polymorphic_path(object)
           
           if xeditable? and can?(:edit, object)
-            model_param = object.class.name.split('::').last.underscore
+            model = object.class.name.split('::').last.underscore
             klass = options[:nested] ? object.class.const_get(options[:nested].to_s.singularize.capitalize) : object.class
             
             tag   = options.fetch(:tag, 'span')
             title = options.fetch(:title, klass.human_attribute_name(method))
             data  = { 
               type:   options.fetch(:type, 'text'), 
-              model:  model_param, 
+              model:  model, 
               name:   method, 
               value:  value, 
-              url:    data_url, 
+              url:    url, 
               nested: options[:nested], 
               nid:    options[:nid]
             }.merge options.fetch(:data, {})
