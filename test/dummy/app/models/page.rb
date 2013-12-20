9c275a6459bddcba6ff81a446caa71357d37ed5c
JiriKolarik <jiri.kolarik@imin.cz>
2013-12-20 10:59:06 +0100

    Example with error handling

--- test/dummy/app/models/page.rb
+++ test/dummy/app/models/page.rb
@@ -1,4 +1,5 @@
 class Page < ActiveRecord::Base
   translates :title
   accepts_nested_attributes_for :translations, allow_destroy: true
+  validates :name, length: { minimum: 5 }, allow_blank: true
 end
