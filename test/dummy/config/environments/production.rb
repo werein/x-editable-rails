82c42afbcb6e23a903de82162fae1461730a60b0
Jiri Kolarik <jiri.kolarik@wereinhq.com>
2016-11-06 13:00:49 +0100

    remove digest from production

--- test/dummy/config/environments/production.rb
+++ test/dummy/config/environments/production.rb
@@ -27,10 +27,10 @@ Dummy::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
-  config.assets.digest = true
+  config.assets.digest = false
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
